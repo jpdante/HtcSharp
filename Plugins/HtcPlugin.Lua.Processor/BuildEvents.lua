fdd75c749a26e39f76fc343a3cb54abce1da2a4a
João Pedro Caires <joaopdante@gmail.com>
2020-02-14 12:28:55 -0300

    Update BuildEvents.lua

--- Plugins/HtcPlugin.Lua.Processor/BuildEvents.lua
+++ Plugins/HtcPlugin.Lua.Processor/BuildEvents.lua
@@ -16,6 +16,6 @@ elseif args[2] == "Release" then
 	    Directory.createDirectory(pluginsPath)
 	end
 	File.copy(args[4], Path.combine(args[3], [[HtcSharp.Server/bin/Release/plugins]] .. args[5]), true)
-	File.copy(Path.combine(args[3], [[Plugins/HtcPlugin.Lua.MyProcessorSQL/bin/Release]], "MoonSharp.Interpreter.dll"), Path.combine(args[3], [[HtcSharp.Server/bin/Release/plugins]], "MoonSharp.Interpreter.dll"), true)
+	File.copy(Path.combine(args[3], [[Plugins/HtcPlugin.Lua.Processor/bin/Release]], "MoonSharp.Interpreter.dll"), Path.combine(args[3], [[HtcSharp.Server/bin/Release/plugins]], "MoonSharp.Interpreter.dll"), true)
 end
 print("[LuaBuildEvents] Finishing HtcPlugin.Lua.Processor PostBuild\n")
