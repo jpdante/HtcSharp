84ade62a48b637cc21e9e35342973ff61ab76cf3
João Pedro Caires <joaopdante@gmail.com>
2020-02-21 17:29:59 -0300

    Removing unused classes

--- HtcSharp.HttpModule2/Net/Connections/Features/IConnectionIdFeature.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-
-namespace HtcSharp.HttpModule2.Net.Connections.Features {
-    public interface IConnectionIdFeature {
-        string ConnectionId { get; set; }
-    }
-}
