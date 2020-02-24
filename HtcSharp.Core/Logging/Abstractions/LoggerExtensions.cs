3fd8ca12d6823d2c56dbb029807bd50328681e4a
João Pedro Caires <joaopdante@gmail.com>
2020-02-24 00:51:55 -0300

    Update plugins, Add more logging extensions

--- HtcSharp.Core/Logging/Abstractions/LoggerExtensions.cs
+++ HtcSharp.Core/Logging/Abstractions/LoggerExtensions.cs
@@ -27,5 +27,28 @@ namespace HtcSharp.Core.Logging.Abstractions {
             logger.Log(LogLevel.Trace, obj, ex);
         }
 
+        public static void LogDebug(this ILogger logger, object obj) {
+            LogDebug(logger, obj, null);
+        }
+
+        public static void LogInfo(this ILogger logger, object obj) {
+            LogInfo(logger, obj, null);
+        }
+
+        public static void LogWarn(this ILogger logger, object obj) {
+            LogWarn(logger, obj, null);
+        }
+
+        public static void LogError(this ILogger logger, object obj) {
+            LogError(logger, obj, null);
+        }
+
+        public static void LogFatal(this ILogger logger, object obj) {
+            LogFatal(logger, obj, null);
+        }
+
+        public static void LogTrace(this ILogger logger, object obj) {
+            LogTrace(logger, obj, null);
+        }
     }
 }
--- Plugins/HtcPlugin.Lua.MySQL/MySQLRegister.cs
+++ Plugins/HtcPlugin.Lua.MySQL/MySQLRegister.cs
@@ -1,24 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Reflection;
-using System.Text;
-using HtcPlugin.Lua.MySql.Models;
+﻿using HtcPlugin.Lua.MySql.Models;
 using HtcSharp.Core;
-using HtcSharp.Core.Interfaces.Plugin;
-using HtcSharp.Core.Logging;
+using HtcSharp.Core.Logging.Abstractions;
+using HtcSharp.Core.Plugin.Abstractions;
+using Microsoft.Extensions.Logging;
 using MoonSharp.Interpreter;
 
 namespace HtcPlugin.Lua.MySql {
-    public class MySQLRegister {
-        private static readonly Logger Logger = LogManager.GetILog(MethodBase.GetCurrentMethod().DeclaringType);
+    public class MySqlRegister {
         private readonly Processor.LuaProcessor _luaProcessor;
 
-        public MySQLRegister(IHtcPlugin pluginContext) {
-            foreach (var plugin in HtcServer.Context.PluginsManager.GetPlugins) 
-                if (plugin is Processor.LuaProcessor processor) _luaProcessor = processor;
+        public MySqlRegister(LuaMySql pluginContext) {
+            // TODO: Fix that
+            //foreach (var plugin in HtcServer.Context.PluginsManager.GetPlugins)
+                //if (plugin is Processor.LuaProcessor processor) _luaProcessor = processor;
             if (_luaProcessor == null) {
-                Logger.Error("Failed to initialize HtcPlugin.Lua.MySql, dependency context not found.");
-                HtcServer.Context.PluginsManager.UnloadPlugin(pluginContext);
+                pluginContext.Logger.LogError("Failed to initialize HtcPlugin.Lua.MySql, dependency context not found.");
+                //HtcServer.Context.PluginsManager.UnloadPlugin(pluginContext);
                 return;
             }
             UserData.RegisterType<LuaSql>();
