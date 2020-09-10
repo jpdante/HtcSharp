dc268d8dbe4de57bcd0fdceb631c1c704fe01065
João Pedro Caires <joaopdante@gmail.com>
2020-09-10 20:31:00 -0300

    Update ISession.cs

--- Modules/HtcSharp.HttpModule/Http.Features/ISession.cs
+++ Modules/HtcSharp.HttpModule/Http.Features/ISession.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -12,36 +13,59 @@ namespace HtcSharp.HttpModule.Http.Features {
     // SourceTools-End
     public interface ISession {
         /// <summary>
-        /// Indicate whether the current session is valid.
+        /// Indicate whether the current session has loaded.
         /// </summary>
-        bool IsValid { get; }
+        bool IsAvailable { get; }
 
         /// <summary>
-        /// A unique identifier for the current session.
+        /// A unique identifier for the current session. This is not the same as the session cookie
+        /// since the cookie lifetime may not be the same as the session entry lifetime in the data store.
         /// </summary>
         string Id { get; }
 
         /// <summary>
-        /// Custom data for the current session.
+        /// Enumerates all the keys, if any.
         /// </summary>
-        object Data { get; }
+        IEnumerable<string> Keys { get; }
 
         /// <summary>
-        /// Load the session.
+        /// Load the session from the data store. This may throw if the data store is unavailable.
         /// </summary>
         /// <returns></returns>
         Task LoadAsync(CancellationToken cancellationToken = default(CancellationToken));
 
         /// <summary>
-        /// Create session.
+        /// Store the session in the data store. This may throw if the data store is unavailable.
         /// </summary>
         /// <returns></returns>
-        Task CreateAsync(object data = null, CancellationToken cancellationToken = default(CancellationToken));
+        Task CommitAsync(CancellationToken cancellationToken = default(CancellationToken));
 
         /// <summary>
-        /// Delete session.
+        /// Retrieve the value of the given key, if present.
         /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
         /// <returns></returns>
-        Task DeleteAsync(CancellationToken cancellationToken = default(CancellationToken));
+        bool TryGetValue(string key, out byte[] value);
+
+        /// <summary>
+        /// Set the given key and value in the current session. This will throw if the session
+        /// was not established prior to sending the response.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        void Set(string key, byte[] value);
+
+        /// <summary>
+        /// Remove the given key from the session if present.
+        /// </summary>
+        /// <param name="key"></param>
+        void Remove(string key);
+
+        /// <summary>
+        /// Remove all entries from the current session, if any.
+        /// The session cookie is not removed.
+        /// </summary>
+        void Clear();
     }
 }
