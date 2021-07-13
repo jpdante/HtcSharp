7e3f6a053e16517ab354ea48e8e4437bd4d6ef14
João Pedro Caires <joaopdante@tryhosting.com.br>
2021-07-13 09:43:42 -0300

    Add version and update post build events

---
+++ HtcSharp/Version.cs
@@ -0,0 +1,11 @@
+﻿namespace HtcSharp {
+    public static class Version {
+
+        public static int Major = 0;
+
+        public static int Minor = 1;
+
+        public static int Patch = 0;
+
+    }
+}
--- Modules/HtcSharp.HttpModule/PostBuild.BuildEvents.lua
+++ Modules/HtcSharp.HttpModule/PostBuild.BuildEvents.lua
@@ -3,9 +3,15 @@ require("lua.io")
 
 -- Filename and directory blacklist
 function isBlackListed(path)
-  if path == [[CorePluginLoader.dll]] then return true
-  elseif path == [[HtcSharp.Core.dll]] then return true
+  if path == [[HtcSharp.Core.dll]] then return true
   elseif path == [[HtcSharp.Core.pdb]] then return true
+  elseif path == [[HtcSharp.Logging.dll]] then return true
+  elseif path == [[HtcSharp.Logging.pdb]] then return true
+  elseif path == [[HtcSharp.Abstractions.dll]] then return true
+  elseif path == [[HtcSharp.Abstractions.pdb]] then return true
+  elseif path == [[HtcSharp.Shared.dll]] then return true
+  elseif path == [[HtcSharp.Shared.pdb]] then return true
+  elseif path == [[ref]] then return true
   else return false
   end
 end
@@ -24,7 +30,7 @@ outputDirectories = Directory.getDirectories(outputDirectory, "*", SearchOption.
 for key,value in ipairs(outputDirectories) do
   directoryReplace = value:gsub(outputDirectory, "")
   if isBlackListed(directoryReplace) == true then
-    print("Blacklisted directory: " .. Path.getFileName(directoryReplace) .. "\n")
+    print("Blacklisted directory: " .. value .. "\n")
     goto continue
   end
   fixedDirectory = Path.combine(pluginsPath, directoryReplace)
@@ -32,11 +38,26 @@ for key,value in ipairs(outputDirectories) do
     print("Creating directory: " .. fixedDirectory .. "\n")
     Directory.createDirectory(fixedDirectory)
   end
+
+  -- Copy files
+  outputFiles = Directory.getFiles(value, "*.*", SearchOption.TopDirectoryOnly)
+  for key2,value2 in ipairs(outputFiles) do
+    fileNameReplace = value2:gsub(outputDirectory, "")
+    if isBlackListed(Path.getFileName(fileNameReplace)) == true then
+      print("Blacklisted file: " .. Path.getFileName(fileNameReplace) .. "\n")
+      goto continue2
+    end
+    fixedFileName = Path.combine(pluginsPath, fileNameReplace)
+    print("Copying file: " .. fixedFileName .. "\n")
+    File.copy(value2, fixedFileName, true)
+    ::continue2::
+  end
+
   ::continue::
 end
 
 -- Copy files
-outputFiles = Directory.getFiles(outputDirectory, "*.*", SearchOption.AllDirectories)
+outputFiles = Directory.getFiles(outputDirectory, "*.*", SearchOption.TopDirectoryOnly)
 for key,value in ipairs(outputFiles) do
   fileNameReplace = value:gsub(outputDirectory, "")
   if isBlackListed(Path.getFileName(fileNameReplace)) == true then
@@ -48,5 +69,6 @@ for key,value in ipairs(outputFiles) do
   File.copy(value, fixedFileName, true)
   ::continue::
 end
+
 
 print("[LuaBuildEvents] Finishing HtcSharp.HttpModule PostBuild\n")
