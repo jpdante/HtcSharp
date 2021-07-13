e847adb31233d92f3e7b20d1f9349694ab6de211
João Pedro Caires <joaopdante@tryhosting.com.br>
2021-07-13 10:42:02 -0300

    Test basic http engine

--- Modules/HtcSharp.HttpModule/HttpEngine.cs
+++ Modules/HtcSharp.HttpModule/HttpEngine.cs
@@ -1,24 +1,39 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
-using Microsoft.Extensions.Logging;
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using HtcSharp.Logging;
+using Microsoft.AspNetCore.Hosting;
 
 namespace HtcSharp.HttpModule {
     public class HttpEngine {
 
-        public string Name => "Http";
+        private readonly ILogger Logger = LoggerManager.GetLogger(MethodBase.GetCurrentMethod()?.DeclaringType);
 
-        private KestrelServer _kestrelServer;
+        private IWebHost _webHost;
 
-        public async Task Load() {
+        public HttpEngine() {
 
+        }
+
+        public Task Load() {
+            Logger.LogInfo("Loading HttpEngine...");
+            var webHostBuilder = new WebHostBuilder();
+            _webHost = webHostBuilder.UseKestrel(options => {
+                options.ListenAnyIP(80, listenOptions => {
+                    listenOptions.NoDelay = true;
+                });
+            }).Build();
+            return Task.CompletedTask;
         }
 
         public async Task Start() {
-    
+            Logger.LogInfo("Starting HttpEngine...");
+            await _webHost.StartAsync();
         }
 
         public async Task Stop() {
-     
+            Logger.LogInfo("Stopping HttpEngine...");
+            await _webHost.StopAsync();
         }
     }
 }
