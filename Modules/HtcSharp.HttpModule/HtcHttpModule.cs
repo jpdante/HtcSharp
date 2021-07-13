bffc601c3fa72d6f3ab827103cb2e1b015378641
João Pedro Caires <joaopdante@tryhosting.com.br>
2021-07-13 10:41:50 -0300

    Test basic http engine

--- Modules/HtcSharp.HttpModule/HtcHttpModule.cs
+++ Modules/HtcSharp.HttpModule/HtcHttpModule.cs
@@ -1,22 +1,29 @@
-﻿using System.Threading.Tasks;
+﻿using System.Reflection;
+using System.Threading.Tasks;
 using HtcSharp.Abstractions;
+using HtcSharp.Logging;
 
 namespace HtcSharp.HttpModule {
     public class HtcHttpModule : IModule {
 
+        private readonly ILogger Logger = LoggerManager.GetLogger(MethodBase.GetCurrentMethod()?.DeclaringType);
+
         public string Name => "HtcHttp";
         public string Version => "0.1.0";
 
-        public Task Load() {
-            return Task.CompletedTask;
+        private HttpEngine _httpEngine;
+
+        public async Task Load() {
+            _httpEngine = new HttpEngine();
+            await _httpEngine.Load();
         }
 
-        public Task Enable() {
-            return Task.CompletedTask;
+        public async Task Enable() {
+            await _httpEngine.Start();
         }
 
-        public Task Disable() {
-            return Task.CompletedTask;
+        public async Task Disable() {
+            await _httpEngine.Stop();
         }
 
         public bool IsCompatible(int htcMajor, int htcMinor, int htcPatch) {
