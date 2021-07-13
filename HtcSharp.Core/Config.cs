9bb553d747339a0593254bafd46e1688e6766226
João Pedro Caires <joaopdante@tryhosting.com.br>
2021-07-13 09:50:45 -0300

    Update Json property naming on config

--- HtcSharp.Core/Config.cs
+++ HtcSharp.Core/Config.cs
@@ -1,8 +1,12 @@
-﻿namespace HtcSharp.Core {
+﻿using System.Text.Json.Serialization;
+
+namespace HtcSharp.Core {
     public class Config {
 
+        [JsonPropertyName("modulesPath")]
         public string ModulesPath { get; set; } = "./modules/";
 
+        [JsonPropertyName("pluginsPath")]
         public string PluginsPath { get; set; } = "./plugins/";
 
     }
