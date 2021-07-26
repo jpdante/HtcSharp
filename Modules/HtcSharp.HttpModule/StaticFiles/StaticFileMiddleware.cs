024a85ab68608d8d1ec7fafeebd385c2425d4934
João Pedro Caires <joaopdante@tryhosting.com.br>
2021-07-26 19:09:58 -0300

    Add StaticFileContext and ContentType

--- Modules/HtcSharp.HttpModule/StaticFiles/StaticFileMiddleware.cs
+++ Modules/HtcSharp.HttpModule/StaticFiles/StaticFileMiddleware.cs
@@ -1,24 +1,59 @@
-﻿using System.Threading.Tasks;
-using HtcSharp.HttpModule.Abstractions;
+﻿using System;
+using System.Threading.Tasks;
 using HtcSharp.HttpModule.Http;
-using HtcSharp.HttpModule.Middleware;
+using Microsoft.AspNetCore.Http;
+using IMiddleware = HtcSharp.HttpModule.Abstractions.IMiddleware;
+using RequestDelegate = HtcSharp.HttpModule.Middleware.RequestDelegate;
 
 namespace HtcSharp.HttpModule.StaticFiles {
     public class StaticFileMiddleware : IMiddleware {
 
         private readonly RequestDelegate _next;
 
-        public StaticFileMiddleware(RequestDelegate next) {
+        private readonly string _path;
+
+        public StaticFileMiddleware(RequestDelegate next, string path) {
             _next = next;
+            _path = path;
         }
 
-        public async Task Invoke(HtcHttpContext httpContext) {
-            await TryServeFile(httpContext);
-            await _next(httpContext);
+        public Task Invoke(HtcHttpContext httpContext) {
+            if (!IsHeadOrGet(httpContext)) {
+            } else if (!GetPath(httpContext, "", out var subPath)) {
+            } else if (!GetContentType(subPath, out string contentType)) {
+            } else {
+                return TryServeFile(httpContext, subPath, contentType);
+            }
+            return _next(httpContext);
         }
 
-        public async Task TryServeFile(HtcHttpContext httpContext) {
+        private static bool IsHeadOrGet(HttpContext httpContext) {
+            return HttpMethods.IsHead(httpContext.Request.Method) || HttpMethods.IsGet(httpContext.Request.Method);
+        }
 
+        private static bool GetPath(HttpContext context, PathString matchUrl, out PathString subPath, bool forDirectory = false) {
+            var path = context.Request.Path;
+
+            if (forDirectory && !PathEndsInSlash(path)) {
+                path += new PathString("/");
+            }
+
+            return path.StartsWithSegments(matchUrl, out subPath);
+        }
+
+        private static bool PathEndsInSlash(PathString path) {
+            return path.Value.EndsWith("/", StringComparison.Ordinal);
+        }
+
+        private static bool GetContentType(PathString subPath, out string contentType) {
+            var rawContentType = ContentType.CUSTOM_BINARY.FromExtension(subPath.Value);
+            if (rawContentType == 0) rawContentType = ContentType.CUSTOM_BINARY;
+            contentType = rawContentType.ToValue();
+            return true;
+        }
+
+        public async Task TryServeFile(HtcHttpContext httpContext, PathString subPath, string contentType) {
+            
         }
     }
 }
