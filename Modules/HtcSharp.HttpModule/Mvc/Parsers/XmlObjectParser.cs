22501bf8128a130d6aed1881a89ec186d8f56682
João Pedro Caires <joaopdante@tryhosting.com.br>
2021-08-02 15:24:33 -0300

    Add XmlObjectParser/XmlSerializer bug comment

--- Modules/HtcSharp.HttpModule/Mvc/Parsers/XmlObjectParser.cs
+++ Modules/HtcSharp.HttpModule/Mvc/Parsers/XmlObjectParser.cs
@@ -8,6 +8,10 @@ namespace HtcSharp.HttpModule.Mvc.Parsers {
     public class XmlObjectParser : IHttpObjectParser {
 
         public Task<IHttpObject> Parse(HtcHttpContext httpContext, Type objectType) {
+            // Failure to deserialize is a known C# bug
+            // This is caused by the AssemblyLoadContext, there is no workaround for now, it is planned for .NET 6 Preview 2
+            // https://github.com/dotnet/runtime/issues/1388
+            // Possible solution: Use a custom XmlSerializer or external library?
             var xmlSerializer = new XmlSerializer(objectType);
             return Task.FromResult((IHttpObject) xmlSerializer.Deserialize(httpContext.Request.Body));
         }
