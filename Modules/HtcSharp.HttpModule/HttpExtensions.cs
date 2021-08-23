457e855cf3b06424acb4be6a10702d3f018162b8
João Pedro Caires <joaopdante@tryhosting.com.br>
2021-08-23 13:52:48 -0300

    Create HttpExtensions

---
+++ Modules/HtcSharp.HttpModule/HttpExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace HtcSharp.HttpModule {
+    public static class HttpExtensions {
+
+        public static Task WriteJsonAsync(this HttpResponse httpResponse, object obj, JsonSerializerOptions serializerOptions = default, CancellationToken cancellationToken = default) {
+            return JsonSerializer.SerializeAsync(httpResponse.Body, obj, serializerOptions, cancellationToken);
+        }
+
+        public static Task WriteJsonAsync(this HttpResponse httpResponse, object obj, Type type, JsonSerializerOptions serializerOptions = default, CancellationToken cancellationToken = default) {
+            return JsonSerializer.SerializeAsync(httpResponse.Body, obj, type, serializerOptions, cancellationToken);
+        }
+
+    }
+}
